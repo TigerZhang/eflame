9d2bce87ad5b3125271fc5f3c9dbe13ff6b74c66
Zhang Hu <iamzhanghu@gmail.com>
2015-12-04 19:23:11 +0800

    search running beam by ps

--- eflame.sh
+++ eflame.sh
@@ -1,39 +1,107 @@
 #!/usr/bin/env bash
 # Copyright yunba.io 2015
+#
+# tool for launching eflame2
+#
 
-BEAMPROCS=`ps ax | grep -v grep | grep beam | grep setcookie`
-LINECOUNT=`echo $BEAMPROCS | wc -l`
+#BEAMPROCS=`ps ax | grep -v grep | grep beam | grep setcookie`
+#LINECOUNT=`echo $BEAMPROCS | wc -l`
 
-declare -a NODENAMES
-declare -a COOKIES
+declare -i seconds=10
 
-i=0
-#for PROC in `ps ax | grep -v grep | grep beam | grep setcookie`
-while read $PROC;
-do
-    COOKIES[i]=`echo ${BEAMPROCS} | sed 's/.*-setcookie \([a-zA-Z0-9]*\).*/\1/'`
-    NODENAMES[i]=`echo ${BEAMPROCS} | sed 's/.*-name \([a-zA-Z0-9.@]*\).*/\1/'`
-    i=$((i+1))
-done < ${
+function search_running_beams() {
+    TEMPFILE=`mktemp`
 
-echo $i
-echo $COOKIES
-echo $NODENAMES
+    #for PROC in `ps ax | grep -v grep | grep beam | grep setcookie`
+    ps ax | grep -v grep | grep beam | grep setcookie | while read PROC;
+    do
+        COOKIE=`echo ${PROC} | sed 's/.*-setcookie \([a-zA-Z0-9]*\).*/\1/'`
+        NodeName=`echo ${PROC} | sed 's/.*-name \([a-zA-Z0-9.@]*\).*/\1/'`
+        echo $NodeName $COOKIE
+    done > $TEMPFILE
 
-exit 0
+    echo $TEMPFILE
+    LINECOUNT=`wc -l $TEMPFILE | awk '{print $1}'`
+    if [ "${LINECOUNT}" == 1 ]; then
+        node_name=`cat $TEMPFILE | cut -d " " -f 1`
+        cookie=`cat $TEMPFILE | cut -d " " -f 2`
+    else
+        echo "found running beams(name, cookie):"
+        cat $TEMPFILE
+        echo
+    fi
+}
 
-if [ $LINECOUNT -eq 1 ]; then
-    COOKIE=`echo ${BEAMPROCS} | sed 's/.*-setcookie \([a-zA-Z0-9]*\).*/\1/'`
+function show_help() {
+    echo "$0 [-n <node_name>] [-c <cookie>] [-s <seconds>]"
+}
+
+function get_opts() {
+    # A POSIX variable
+    OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+    # Initialize our own variables:
+    node_name=""
+    cookie=""
+
+    while getopts "h?vn:c:s:" opt; do
+#        echo "opt: $opt"
+#        echo "OPTARGS: $OPTARG"
+        case "$opt" in
+        h|\?)
+            show_help
+            exit 0
+            ;;
+        v)  verbose=1
+            ;;
+        n)  node_name=$OPTARG
+            ;;
+        c)  cookie=$OPTARG
+            ;;
+        s)  seconds=$OPTARG
+            ;;
+        esac
+    done
+
+    shift $((OPTIND-1))
+
+    [ "$1" = "--" ] && shift
+
+    echo "node_name: ${node_name} cookie: ${cookie} seconds: ${seconds}"
+}
+
+function do_eflame() {
+    echo "eflame launch"
+    escript eflame_launch.erl ${node_name} ${cookie} ${seconds}
+
+#    rm $TEMPFILE
+
+    OUTPUT=output-${node_name}.svg
+    OUTPUT_NO_SLEEP=output-no-sleep-${node_name}.svg
+
+    rm ${OUTPUT}
+    rm ${OUTPUT_NO_SLEEP}
+
+    echo "generate output.svg"
+    cat /tmp/ef.test.0.out | ./flamegraph.riak-color.pl > ${OUTPUT}
+    echo "generate output-no-sleep.svg"
+    grep -v 'SLEEP' /tmp/ef.test.0.out | ./flamegraph.riak-color.pl > ${OUTPUT_NO_SLEEP}
+    echo "open output.svg"
+    open ${OUTPUT}
+    echo "open output-no-sleep.svg"
+    open ${OUTPUT_NO_SLEEP}
+}
+
+# main
+
+get_opts $*
+
+if [ -z ${node_name} ] && [ -z ${cookie} ]; then
+    search_running_beams
 fi
 
-echo "eflame launch"
-escript eflame_launch.erl plumtree3@127.0.0.1 plumtree 5
-
-echo "generate output.svg"
-cat /tmp/ef.test.0.out | ./flamegraph.riak-color.pl > output.svg
-echo "generate output-no-sleep.svg"
-grep -v 'SLEEP' /tmp/ef.test.0.out | ./flamegraph.riak-color.pl > output-no-sleep.svg
-echo "open output.svg"
-open output.svg
-echo "open output-no-sleep.svg"
-open output-no-sleep.svg
+if [ -z ${node_name} ] || [ -z ${cookie} ]; then
+    show_help
+else
+    do_eflame
+fi
